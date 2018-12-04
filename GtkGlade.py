bfe843be6e710fd56894ea12638507612c1a255c
neslihanaydin <neslihanaydin43@gmail.com>
2018-12-04 19:29:41 +0300

    label elementi xml hale getirildi

--- GtkGlade.py
+++ GtkGlade.py
@@ -1,42 +1,38 @@
 import gi, sys
 gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
+import gtkWindow as gw
 
-#def whatis(obj):
-#    props = dir(obj)
-#    print(type(obj))
-#    print("\t"+"\t\n\t".join(props))
+whatis = lambda obj: print(type(obj), "\n\t"+"\n\t".join(dir(obj))) # for obj attributes
 
-whatis = lambda obj: print(type(obj), "\n\t"+"\n\t".join(dir(obj)))
 
 class aClassThatHandleGtkSignals:
 
-    window = Gtk.Window()
+    window = Gtk.Window() # application window
     def onQuit(self, *a, **kv):
         print("exit")
         sys.exit(0)
-    def newProject(self, *a, **kv):
-        print("CLICKED")
-        
+    def newProject(self, *a, **kv): # newProject button on clicked
         aClassThatHandleGtkSignals.window.show_all()
         Gtk.main()
-    def saveProject(self, *a, **kv):
-    	print("metoda girdim")
-    	if(aClassThatHandleGtkSignals.window!=0):
-    		print("ife girdin")
-    		import gtkWindow as gw
+    def saveProject(self, *a, **kv): #saveProject button on clicked
+    	if(aClassThatHandleGtkSignals.window.get_name()=="GtkWindow"): #this means if the projectName is not saved
+    		# FileChooserDialog settings for SAVE
     		self.dialog = Gtk.FileChooserDialog("please", None, Gtk.FileChooserAction.SAVE,(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.ACCEPT))
     		aClassThatHandleGtkSignals.add_filters(None,self.dialog)
-    		response = self.dialog.run()
+    		response = self.dialog.run() # Show FileChooserDialog
     		Gtk.FileChooser.set_do_overwrite_confirmation(self.dialog, True)
     		if response == Gtk.ResponseType.ACCEPT:
     			print("Open clicked")
     			print("File selected: " + self.dialog.get_filename())
     		elif response == Gtk.ResponseType.CANCEL:
     			print("Cancel clicked")
-    		gw.glade_create_files(self.dialog.get_filename(),self.dialog.get_filename().split('/')[-1])
-    		self.dialog.destroy()
-    def add_filters(self, dialog):
+    		save_name=self.dialog.get_filename().split('/')[-1] # save_name is the name of the saved project
+    		gw.glade_create_files(self.dialog.get_filename(),save_name) #glade_create_files function on gtkWindow.py
+    		aClassThatHandleGtkSignals.window.set_name(save_name) # application window name
+    		aClassThatHandleGtkSignals.window.set_title(save_name) # application window title name
+    		self.dialog.destroy() # FileChooserDialog closing
+    def add_filters(self, dialog): # FileChooserDialogs adding filter function
         filter_text = Gtk.FileFilter()
         filter_text.set_name("Text files")
         filter_text.add_mime_type("text/plain")
@@ -52,25 +48,29 @@ class aClassThatHandleGtkSignals:
         filter_any.add_pattern("*")
         dialog.add_filter(filter_any)
 
-    def labelEkle(self, *a, **kv):
+    def labelEkle(self, *a, **kv): # labelEkle button is clicked
         print("Label Adding Start ! ")
         self.label = Gtk.Label()
-        self.label.set_label("Label Eklendi !")
-        aClassThatHandleGtkSignals.window.add(self.label)
+        self.label.set_label("label")
+        aClassThatHandleGtkSignals.window.add(self.label) # add label to application window
         aClassThatHandleGtkSignals.window.show_all()
-    def butonEkle(self, *a, **kv):
+        if aClassThatHandleGtkSignals.window.get_name()!="GtkWindow": # if Project was saved
+        	gw.addLabel(aClassThatHandleGtkSignals.window.get_name()) # addLabel function on gtkWindow.py ,this function saves in xml format,
+        
+
+    def butonEkle(self, *a, **kv): # butonEkle button is clicked #NOT COMPLETED YET
         print("Buton Adding Start ! ")
         self.buton = Gtk.Button(label = "Click Me !")
         self.buton.connect("clicked",MainWindow.button_clicked)
         aClassThatHandleGtkSignals.window.add(self.buton)
         aClassThatHandleGtkSignals.window.show_all()
-    def gridEkle(self, *a, **kv):
+    def gridEkle(self, *a, **kv): #NOT COMPLETED YET
     	print("Grid ekleniyor")
     	self.grid = Gtk.Grid()
     	aClassThatHandleGtkSignals.window.add(self.grid)
     	aClassThatHandleGtkSignals.window.show_all()
         
-class MainWindow(Gtk.Window):
+class MainWindow(Gtk.Window): #TEST
 
     def __init__(self):
         Gtk.Window.__init__(self, title = "Button Clicker")
@@ -82,9 +82,10 @@ class MainWindow(Gtk.Window):
     def button_clicked(self, widget):
         print("GameTime")
 
+#Builder for Gtk and Glade relationship
 abuilder = Gtk.Builder()
 abuilder.add_from_file("forms1.glade")
-abuilder.connect_signals(aClassThatHandleGtkSignals)
+abuilder.connect_signals(aClassThatHandleGtkSignals) #Formda gerceklesen olaylar
 ourForm1 = abuilder.get_object("form1")
 ourForm1.show_all()
 Gtk.main()
--- gtkWindow.py
+++ gtkWindow.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import gi, sys
 gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
-def glade_create_files(save_name,project_name):
+def glade_create_files(save_name,project_name): #glade format requires code
 	a= ET.Element('interface')
 	b=ET.SubElement(a,'requires')
 	b.set('lib', 'gtk+')
@@ -15,7 +15,34 @@ def glade_create_files(save_name,project_name):
 	d.text="False"
 	e= ET.SubElement(c,'child')
 	ET.SubElement(e,'placeholder')
-	send = ET.tostring(a,encoding='utf8',method='xml').decode()
+	send = ET.tostring(a,encoding='utf8',method='xml').decode() #send variable for saveFile
 	ET.dump(a)
-	yn = open(save_name+".glade", "w")
-	yn.write(send)
+	yn = open(save_name+".glade", "w") # new file create
+	yn.write(send) #xml format file writing
+	yn.close()
+def addLabel(project_name): #addLabel buton is clicked ,this function add xml file,
+	yn = open(project_name+".glade")
+	ynoku = yn.read()
+	root = ET.fromstring(ynoku)
+	print(root.tag)
+	temp=0
+	for child in root.findall('object'):
+		for i in child.attrib.keys():
+			if i=="id":
+				if child.attrib["id"]==project_name:
+					new_child = ET.Element("child")
+					new_label = ET.SubElement(new_child,"object")
+					new_label.set("class","GtkLabel")
+					property_1=ET.SubElement(new_label,"property")
+					property_1.set("name","visible")
+					property_1.text="True"
+					property_2=ET.SubElement(new_label,"property")
+					property_2.set("name","can_focus")
+					property_2.text="False"
+					property_3=ET.SubElement(new_label,"property")
+					property_3.set("name","label")
+					property_3.set("translatable","yes")
+					property_3.text="label"
+					child.append(new_child)
+	yn1= open(project_name+".glade","w")	
+	yn1.write(ET.tostring(root,encoding='utf8',method='xml').decode())
